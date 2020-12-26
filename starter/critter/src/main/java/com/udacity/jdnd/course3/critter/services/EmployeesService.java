dd120c1fb10818c1b7905bfa83aad4fbf80fecd5
marchingheng <yhcai@umich.edu>
2020-12-25 18:25:35 -0600

    build the controller layer; add @Servive to all the service class; change the pets in Customer.java from Set<Pet> to List<Pet>.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/EmployeesService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/EmployeesService.java
@@ -4,6 +4,7 @@ import com.udacity.jdnd.course3.critter.entities.Employee;
 import com.udacity.jdnd.course3.critter.repositories.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Service
 public class EmployeesService {
     @Autowired
     private EmployeeRepository employeeRepository;
